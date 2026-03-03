eac2874db3f2e014e8df876e15a0054bbd69f3fe
kkmname <kkmaddress@gmail.com>
2026-03-03 20:17:02 +0900

    chore: login sample data

---
+++ src/main/resources/db/migration/local/V99__sample.sql
@@ -0,0 +1,3 @@
+-- account: local / password
+INSERT INTO users (name, email, password, authority) 
+VALUES ('local', 'local@kkmserver.com', '$2a$10$eXYRNe/dsG6beUu9qssy1ufgstm4iNfrTYpu8EUMzhIWWo2xZrrn2', 'ROLE_ADMIN');
